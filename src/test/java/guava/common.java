ac046d7b0affc100111b2acfae294fb31afbcc97
xskui6@163.com <kui!0311>
2016-06-14 11:52:33 +0800

    加入了guava包

--- src/test/java/guava/common.java
+++ src/test/java/guava/common.java
@@ -1,10 +1,20 @@
 package guava;
 
+import static com.google.common.base.Preconditions.*;
 /**
  * Created by shukui.xing
  * date 2016/6/14.
  * desc:
  */
 public class common {
-    
+
+    public static void main(String[] args) {
+        try {
+            checkArgument(false,"falsesssss");
+        }catch (IllegalArgumentException ex){
+            System.out.println(ex.getMessage());
+        }finally {
+            System.out.println("finally");
+        }
+    }
 }
